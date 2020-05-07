25102c70525ca0c80330c261c43b88ed962273fc
Rajesh-Royal <rajeshroyal896@gmail.com>
2020-05-07 16:49:42 +0530

    single page, mobile responsive, css changed

---
+++ assets/js/main-script.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+
+    //autoplay hero section video 
+
+    setTimeout(function () {
+        //document.getElementById("hero-video").play();
+    }, 5000);
+
+    //onscroll change header color 
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $("header").css("background-color", "black");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $("header").css("background-color", "transprent");
+        }
+    });
+
+    //owl carousel settings
+
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+    //autoplay video on hover over an video item
+    var figure = $(".video").hover(hoverVideo, hideVideo);
+
+    function hoverVideo(e) {
+        $('video', this).get(0).play();
+    }
+
+    function hideVideo(e) {
+        $('video', this).get(0).pause();
+        $('video', this).get(0).load();
+    } 
+    
+
+});
+
